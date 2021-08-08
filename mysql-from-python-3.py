84e3c08e22ca6f58ae0ec7c15b95dae95b3298ba
baijuka <baijuka@yahoo.com>
2021-08-08 00:58:20 +0000

    added more py files

---
+++ mysql-from-python-3.py
@@ -0,0 +1,21 @@
+import os
+import datetime
+import pymysql
+
+# Get the username from the Cloud9 workspace
+# (modify this variable if running on another environment)
+username = os.getenv('C9_USER')
+
+# Connect to the database
+connection = pymysql.connect(host='localhost',
+                             user=username,
+                             password='',
+                             db='Chinook')
+
+try:
+    with connection.cursor() as cursor:
+        row = ("Josh", 21, "1990-02-06 23:04:56")
+        cursor.execute("INSERT INTO Friends VALUES (%s, %s, %s);", row)
+        connection.commit()
+finally:
+    connection.close()
